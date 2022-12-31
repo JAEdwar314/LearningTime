6d54a884bb5857b95a6d0757d7e65ed57644fd3f
JAEdwar314 <118127008+JAEdwar314@users.noreply.github.com>
2022-12-31 01:37:21 +0000

    Ownership n Strings

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,10 +1,22 @@
-
+//Memory Management via Ownership, and strings
 
 fn main(){   
-    let mut string = String::from("1. Hello");
-    println!("{}", string);
-    string.push_str("Yr Mom");
-    println!("{}", string);
+    let a = true;
+    let mut Outisde: String;
+    if a == true{
+        let Inside = String::from("I'm inside the if statement");
+        println!("{}", Inside);
+        Outisde = Inside;
+        println!("{}", Outisde);
+        // This first create a string var that then tx's ownership to outside and then prints it
+    }
+
+    if a == true{
+        let Inside = String::from("I'm inside the if statement");
+        Outisde = Inside.clone();
+        println!("{}", Inside);
+        //This initiallized Outside like before but it prints after ownership has been changed from "Inside"
+    }
 
     
 
